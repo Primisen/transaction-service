b40ab8876107842fb2bbe9871c3d84b437e84957
Nadia Shokel <primisen17@gmail.com>
2023-11-12 14:54:53 +0100

    Changed repositories implementation from JpaReppository to CrudRepository

--- src/main/java/com/id_finance/test_task/transaction_service/repository/TransactionRepository.java
+++ src/main/java/com/id_finance/test_task/transaction_service/repository/TransactionRepository.java
@@ -3,14 +3,14 @@ package com.id_finance.test_task.transaction_service.repository;
 import com.id_finance.test_task.transaction_service.dto.LimitExceededTransactionDto;
 import com.id_finance.test_task.transaction_service.entity.Account;
 import com.id_finance.test_task.transaction_service.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
 
     @Query(value = "SELECT * FROM transaction JOIN account ON transaction.account_from_id = account.id JOIN limits ON account.id = limits.account_id WHERE account_from_id=(?1)", nativeQuery = true)
     List<LimitExceededTransactionDto> getLimitExceededTransactions(Account account);
