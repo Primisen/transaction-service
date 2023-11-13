0bf842b11299bf7c70e82e5a25f4c2a88447ff00
Nadia Shokel <primisen17@gmail.com>
2023-11-13 11:02:57 +0100

    Updated getting exchange rate from API functionality

--- src/main/java/com/id_finance/test_task/transaction_service/Main.java
+++ src/main/java/com/id_finance/test_task/transaction_service/Main.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @OpenAPIDefinition(info = @Info(title = "Transaction-service", version = "1.0", description = "Documentation APIs v1.0"))
 @SpringBootApplication
+@EnableScheduling
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
--- src/main/java/com/id_finance/test_task/transaction_service/client/ExchangeRateClient.java
+++ src/main/java/com/id_finance/test_task/transaction_service/client/ExchangeRateClient.java
@@ -1,16 +1,19 @@
-package com.id_finance.test_task.transaction_service.service.impl;
+package com.id_finance.test_task.transaction_service.client;
 
 import com.id_finance.test_task.transaction_service.entity.ExchangeRate;
 import com.id_finance.test_task.transaction_service.repository.ExchangeRateRepository;
-import com.id_finance.test_task.transaction_service.service.ExchangeRateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.concurrent.ExecutionException;
 
+@EnableAsync
 @Service
-public class ExchangeRateServiceImpl implements ExchangeRateService {
+public class ExchangeRateClient {
 
     private final ExchangeRateRepository exchangeRateRepository;
 
@@ -20,12 +23,13 @@ public class ExchangeRateServiceImpl implements ExchangeRateService {
     private static final String API_KEY = "2ebe7a02cccd47f5a0d0e0efbf737053";
 
     @Autowired
-    ExchangeRateServiceImpl(ExchangeRateRepository exchangeRateRepository) {
+    ExchangeRateClient(ExchangeRateRepository exchangeRateRepository) {
         this.exchangeRateRepository = exchangeRateRepository;
     }
 
-    @Override
-    public ExchangeRate updateExchangeRate(String currency) throws ExecutionException, InterruptedException {
+    @Async
+    @Scheduled(cron = "0 0 11 * * *")
+    public void updateExchangeRate() throws ExecutionException, InterruptedException {
 
         WebClient webClient = WebClient.builder()
                 .baseUrl(API_BASE_URL)
@@ -36,14 +40,12 @@ public class ExchangeRateServiceImpl implements ExchangeRateService {
                 .get()
                 .uri(uriBuilder -> uriBuilder
                         .path(API_EXCHANGE_RATE_PATH)
-                        .queryParam(API_EXCHANGE_RATE_QUERY_PARAM, currency)
+                        .queryParam(API_EXCHANGE_RATE_QUERY_PARAM, "KZT/USD")
                         .build())
                 .retrieve()
                 .bodyToMono(ExchangeRate.class)
                 .block();
 
         exchangeRateRepository.save(exchangeRate);
-
-        return exchangeRate;
     }
 }
